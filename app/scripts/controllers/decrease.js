1289d5579d9c293bbd8a303ec75a8d84c203dcac
Liz Abinante <eabinante@gmail.com>
2014-08-10 12:15:18 -0500

    sts, results, errors objects

--- app/scripts/controllers/decrease.js
+++ app/scripts/controllers/decrease.js
@@ -12,4 +12,20 @@ angular.module('knitcalcApp')
     $scope.title = 'Decrease evenly across a row';
     $scope.icon = 'glyphicon-minus';
     $scope.formName = 'decreaseForm';
+
+    var sts = {};
+    sts.starting = null;
+    sts.ending = null;
+    $scope.sts = sts;
+
+    var results = {};
+    results.tooLarge = null;
+    results.tooSmall = null;
+    results.visible = null;
+    $scope.results = results;
+
+    var errors = {};
+    errors.tooSmall = 'The starting number is too small!';
+    errors.tooLarge = 'The difference is too large! Pick an starting number that is less than double the ending number.';
+    $scope.errors = errors;
   });
