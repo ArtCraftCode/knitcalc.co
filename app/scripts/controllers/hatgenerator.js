eaa065756bdcfbdde8c0381d3dbe06047e412aa6
Liz Abinante <eabinante@gmail.com>
2014-08-10 14:25:18 -0500

    yo angular:route hatGenerator

---
+++ app/scripts/controllers/hatgenerator.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name knitcalcApp.controller:HatgeneratorCtrl
+ * @description
+ * # HatgeneratorCtrl
+ * Controller of the knitcalcApp
+ */
+angular.module('knitcalcApp')
+  .controller('HatGeneratorCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
