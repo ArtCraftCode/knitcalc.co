ceddb9cd55f1b12a7047a15280a15b5b1338d381
Liz Abinante <eabinante@gmail.com>
2014-08-12 12:04:08 -0500

    fix a bug where decreases can tell you to k0

--- app/scripts/controllers/decrease.js
+++ app/scripts/controllers/decrease.js
@@ -41,7 +41,13 @@ angular.module('knitcalcApp')
     }
 
     function unbalancedView() {
-      var base = '(k' + ($scope.results.multiSts - 1) + ', k2tog) ' + $scope.results.multiTimes + ' times';
+      var start;
+      if ($scope.results.multiSts === 1) {
+        start = '(';
+      } else {
+        start = '(k' + $scope.results.multiSts - 1 + ', ';
+      }
+      var base = start + 'k2tog) ' + $scope.results.multiTimes + ' times';
       var ending = ' (' + $scope.sts.ending + ' sts total).';
 
       if ($scope.results.remainderTimes === 0) {
