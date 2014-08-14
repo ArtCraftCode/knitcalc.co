24222d36e698e1d7d70b1fc0a3fc4e6b33a0bea3
Liz Abinante <eabinante@gmail.com>
2014-08-14 18:24:44 -0500

    abbreviations array is all fancy in a pretty table

--- app/scripts/controllers/abbreviations.js
+++ app/scripts/controllers/abbreviations.js
@@ -9,9 +9,12 @@
  */
 angular.module('knitcalcApp')
   .controller('AbbreviationsCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.title = 'Legend of Abbreviations';
+
+    $scope.abbreviations = [
+      { 'stitch': 'k', 'definition': 'knit' },
+      { 'stitch': 'p', 'definition': 'purl' },
+      { 'stitch': 'm1', 'definition': 'make one left' },
+      { 'stitch': 'k2tog', 'definition': 'knit two together' },
+      { 'stitch': 'ssk', 'definition': 'slip, slip, knit both slipped stitches together' }];
   });
