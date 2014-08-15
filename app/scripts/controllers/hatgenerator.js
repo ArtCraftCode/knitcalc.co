591da9153c0d0c7473499e1a3499da138b7a45a2
Liz Abinante <eabinante@gmail.com>
2014-08-15 18:02:51 -0500

    transitioning stuff over from ruby

--- app/scripts/controllers/hatgenerator.js
+++ app/scripts/controllers/hatgenerator.js
@@ -43,17 +43,95 @@ angular.module('knitcalcApp')
     ];
 
     $scope.sizes = [
-      { 'value': '13', label: 'Infant, 0-6 months (13 inches / 33 cm)' },
-      { 'value': '15', label: 'Toddler, 6-18 months (15 inches / 38 cm)' },
-      { 'value': '17', label: 'Child\'s small (17 inches / 43 cm)' },
-      { 'value': '20', label: 'Child\'s medium (20 inches / 51 cm)' },
-      { 'value': '21', label: 'Child\'s large or Women\'s small (21 inches / 53 cm)' },
-      { 'value': '22', label: 'Women\'s medium or Men\'s small (22 inches / 56 cm)' },
-      { 'value': '23', label: 'Women\'s large or Men\'s medium (23 inches / 58 cm)' },
-      { 'value': '25', label: 'Men\'s large (25 inches / 63.5 cm)' }
+      { 'value': '13', label: 'Infant, 0-6 months (13 inches / 33 cm)', 'size': 'infant' },
+      { 'value': '15', label: 'Toddler, 6-18 months (15 inches / 38 cm)', 'size': 'toddler' },
+      { 'value': '17', label: 'Child\'s small (17 inches / 43 cm)', 'size': 'child' },
+      { 'value': '20', label: 'Child\'s medium (20 inches / 51 cm)', 'size': 'child' },
+      { 'value': '21', label: 'Child\'s large or Women\'s small (21 inches / 53 cm)', 'size': 'child' },
+      { 'value': '22', label: 'Women\'s medium or Men\'s small (22 inches / 56 cm)', 'size': 'adult' },
+      { 'value': '23', label: 'Women\'s large or Men\'s medium (23 inches / 58 cm)', 'size': 'adult' },
+      { 'value': '25', label: 'Men\'s large (25 inches / 63.5 cm)', 'size': 'adult' }
     ];
+
 
     $scope.generate = function() {
       $scope.results = true;
+      $scope.pattern = {};
+      gauge();
+      radius();
+      slope();
+      squareInches();
+      mutiples();
+      generateText();
     };
+
+    function gauge() {
+      $scope.pattern.rowGauge = $scope.rowGauge;
+      $scope.pattern.stitchGauge = $scope.stitchGauge;
+      $scope.pattern.rowGauge4 = $scope.rowGauge * 4;
+      $scope.pattern.stitchGauge4 = $scope.stitchGauge * 4;
+    }
+
+    function radius() {
+      $scope.pattern.radius = $scope.size/(2*3.142);
+    }
+
+    function slope() {
+      if ($scope.size === 13) {
+        $scope.pattern.slope = 6;
+      } else if ($scope.size === 15) {
+        $scope.pattern.slope = 8.5;
+      } else if ($scope.size >= 21) {
+        $scope.pattern.slope = 14.5;
+      } else {
+        $scope.pattern.slope = 18;
+      }
+    }
+
+    function squareInches() {
+      var one = 3.142 * $scope.pattern.radius * $scope.pattern.slope;
+      var two = 3.142 * $scope.pattern.radius * $scope.pattern.radius;
+      $scope.pattern.squareInches = one + two;
+    }
+
+    function mutiples() {
+      var raw = Math.floor($scope.size * $scope.stitchGauge);
+      var round8 = raw + 8 - (raw % 8);
+      var round9 = raw + 9 - (raw % 9);
+      var min = Math.min(round8, round9);
+
+      if (min % 8 === 0) {
+        $scope.pattern.castOn = min;
+        $scope.pattern.multiple = 8;
+        $scope.pattern.numDecreases = 8;
+      } else if (min % 9 === 0) {
+        $scope.pattern.castOn = min;
+        $scope.pattern.multiple = 9;
+        $scope.pattern.numDecreases = 9;
+      }
+    }
+
+    function generateText() {
+      if ($scope.pattern.multiple === 8) {
+        $scope.pattern.ribbing = '1x1 or 2x2 (k1, p1 or k2, p2)';
+      } else {
+        $scope.pattern.ribbing = '3x3 (k3, p3)';
+      }
+
+      var ribbing, body;
+
+      if ($scope.size === 13) {
+        $scope.pattern.ribbingRows = 6;
+        $scope.pattern.bodyHeight = '';
+      } else if ($scope.size === 15) {
+        $scope.pattern.ribbingRows = 8.5;
+        $scope.pattern.bodyHeight = '';
+      } else if ($scope.size >= 21) {
+        $scope.pattern.ribbingRows = 14.5;
+        $scope.pattern.bodyHeight = '';
+      } else {
+        $scope.pattern.ribbingRows = 18;
+        $scope.pattern.bodyHeight = '';
+      }
+    }
   });
