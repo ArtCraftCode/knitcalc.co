f587afa8605dc0167dccc484c299aa0ce3340923
Liz Abinante <eabinante@gmail.com>
2014-08-15 17:34:34 -0500

    move sizes into controller

--- app/scripts/controllers/hatgenerator.js
+++ app/scripts/controllers/hatgenerator.js
@@ -42,6 +42,17 @@ angular.module('knitcalcApp')
       { 'value': '10', 'label': 'US 15 (10.0 mm)' }
     ];
 
+    $scope.sizes = [
+      { 'value': '13', label: 'Infant, 0-6 months (13 inches / 33 cm)' },
+      { 'value': '15', label: 'Toddler, 6-18 months (15 inches / 38 cm)' },
+      { 'value': '17', label: 'Child\'s small (17 inches / 43 cm)' },
+      { 'value': '20', label: 'Child\'s medium (20 inches / 51 cm)' },
+      { 'value': '21', label: 'Child\'s large or Women\'s small (21 inches / 53 cm)' },
+      { 'value': '22', label: 'Women\'s medium or Men\'s small (22 inches / 56 cm)' },
+      { 'value': '23', label: 'Women\'s large or Men\'s medium (23 inches / 58 cm)' },
+      { 'value': '25', label: 'Men\'s large (25 inches / 63.5 cm)' }
+    ];
+
     $scope.generate = function() {
       $scope.results = true;
     };
