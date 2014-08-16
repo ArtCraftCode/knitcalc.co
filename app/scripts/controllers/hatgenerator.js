81177d32b3d1ce4d672dad2df3aa200323c476b3
Liz Abinante <eabinante@gmail.com>
2014-08-16 10:21:06 -0500

    move list of hat sizes into hat calculator for use with crown decreases

--- app/scripts/controllers/hatgenerator.js
+++ app/scripts/controllers/hatgenerator.js
@@ -22,17 +22,7 @@ angular.module('knitcalcApp')
 
     $scope.needles = needles.list;
     $scope.yarnWeights = yarns.list;
-
-    $scope.sizes = [
-      { 'value': '13', label: 'Infant, 0-6 months (13 inches / 33 cm)' },
-      { 'value': '15', label: 'Toddler, 6-18 months (15 inches / 38 cm)' },
-      { 'value': '17', label: 'Child\'s small (17 inches / 43 cm)' },
-      { 'value': '20', label: 'Child\'s medium (20 inches / 51 cm)' },
-      { 'value': '21', label: 'Child\'s large or Women\'s small (21 inches / 53 cm)' },
-      { 'value': '22', label: 'Women\'s medium or Men\'s small (22 inches / 56 cm)' },
-      { 'value': '23', label: 'Women\'s large or Men\'s medium (23 inches / 58 cm)' },
-      { 'value': '25', label: 'Men\'s large (25 inches / 63.5 cm)' }
-    ];
+    $scope.sizes = hatCalculator.sizes;
 
     $scope.generate = function() {
       $scope.results = true;
--- app/scripts/services/hatcalculator.js
+++ app/scripts/services/hatcalculator.js
@@ -10,6 +10,17 @@
 angular.module('knitcalcApp')
   .factory('hatCalculator', function () {
     return {
+      sizes: [
+        { 'value': '13', label: 'Infant, 0-6 months (13 inches / 33 cm)' },
+        { 'value': '15', label: 'Toddler, 6-18 months (15 inches / 38 cm)' },
+        { 'value': '17', label: 'Child\'s small (17 inches / 43 cm)' },
+        { 'value': '20', label: 'Child\'s medium (20 inches / 51 cm)' },
+        { 'value': '21', label: 'Child\'s large or Women\'s small (21 inches / 53 cm)' },
+        { 'value': '22', label: 'Women\'s medium or Men\'s small (22 inches / 56 cm)' },
+        { 'value': '23', label: 'Women\'s large or Men\'s medium (23 inches / 58 cm)' },
+        { 'value': '25', label: 'Men\'s large (25 inches / 63.5 cm)' }
+      ],
+
       slope: function ($scope) {
         if ($scope.size === 13) {
           $scope.pattern.slope = 6;
