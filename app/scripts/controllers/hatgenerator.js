c31c2523c1f6fd1a54140f577104ef0fdf44028e
Liz Abinante <eabinante@gmail.com>
2014-08-16 09:25:38 -0500

    remove size category from size object because it is not needed; use $scope.pattern.rowGauge to be consistent

--- app/scripts/controllers/hatgenerator.js
+++ app/scripts/controllers/hatgenerator.js
@@ -20,7 +20,6 @@ angular.module('knitcalcApp')
     $scope.rowGauge = 0;
     $scope.stitchGauge = 0;
 
-    // stored values
     $scope.needles = [
       { 'value': '2.0', 'label': 'US 0 (2.0 mm)' },
       { 'value': '2.25', 'label': 'US 1 (2.25 mm)' },
@@ -43,14 +42,14 @@ angular.module('knitcalcApp')
     ];
 
     $scope.sizes = [
-      { 'value': '13', label: 'Infant, 0-6 months (13 inches / 33 cm)', 'size': 'infant' },
-      { 'value': '15', label: 'Toddler, 6-18 months (15 inches / 38 cm)', 'size': 'toddler' },
-      { 'value': '17', label: 'Child\'s small (17 inches / 43 cm)', 'size': 'child' },
-      { 'value': '20', label: 'Child\'s medium (20 inches / 51 cm)', 'size': 'child' },
-      { 'value': '21', label: 'Child\'s large or Women\'s small (21 inches / 53 cm)', 'size': 'child' },
-      { 'value': '22', label: 'Women\'s medium or Men\'s small (22 inches / 56 cm)', 'size': 'adult' },
-      { 'value': '23', label: 'Women\'s large or Men\'s medium (23 inches / 58 cm)', 'size': 'adult' },
-      { 'value': '25', label: 'Men\'s large (25 inches / 63.5 cm)', 'size': 'adult' }
+      { 'value': '13', label: 'Infant, 0-6 months (13 inches / 33 cm)' },
+      { 'value': '15', label: 'Toddler, 6-18 months (15 inches / 38 cm)' },
+      { 'value': '17', label: 'Child\'s small (17 inches / 43 cm)' },
+      { 'value': '20', label: 'Child\'s medium (20 inches / 51 cm)' },
+      { 'value': '21', label: 'Child\'s large or Women\'s small (21 inches / 53 cm)' },
+      { 'value': '22', label: 'Women\'s medium or Men\'s small (22 inches / 56 cm)' },
+      { 'value': '23', label: 'Women\'s large or Men\'s medium (23 inches / 58 cm)' },
+      { 'value': '25', label: 'Men\'s large (25 inches / 63.5 cm)' }
     ];
 
 
@@ -121,23 +120,23 @@ angular.module('knitcalcApp')
       var ribbing, body;
 
       if ($scope.size === 13) {
-        ribbing = Math.floor($scope.rowGauge);
-        body = Math.floor($scope.rowGauge * 2.5);
+        ribbing = Math.floor($scope.pattern.rowGauge);
+        body = Math.floor($scope.pattern.rowGauge * 2.5);
         $scope.pattern.ribbingRows = '1 inch (' + ribbing + ' rows)';
         $scope.pattern.bodyHeight = '2.5 inches (' + body + ' rows )';
       } else if ($scope.size === 15) {
-        ribbing = Math.floor($scope.rowGauge * 1.5);
-        body = Math.floor($scope.rowGauge * 4);
+        ribbing = Math.floor($scope.pattern.rowGauge * 1.5);
+        body = Math.floor($scope.pattern.rowGauge * 4);
         $scope.pattern.ribbingRows = '1.5 inches (' + ribbing + ' rows)';
         $scope.pattern.bodyHeight = '4 inches (' + body + ' rows)';
       } else if ($scope.size >= 21) {
-        ribbing = Math.floor($scope.rowGauge * 1.5);
-        body = Math.floor($scope.rowGauge * 5);
+        ribbing = Math.floor($scope.pattern.rowGauge * 1.5);
+        body = Math.floor($scope.pattern.rowGauge * 5);
         $scope.pattern.ribbingRows = '1.5 inches (' + ribbing + ' rows)';
         $scope.pattern.bodyHeight = '5 inches (' + body + ' rows)';
       } else {
-        ribbing = Math.floor($scope.rowGauge * 2);
-        body = Math.floor($scope.rowGauge * 6.5);
+        ribbing = Math.floor($scope.pattern.rowGauge * 2);
+        body = Math.floor($scope.pattern.rowGauge * 6.5);
         $scope.pattern.ribbingRows = '2 inches (' + ribbing + ' rows)';
         $scope.pattern.bodyHeight = '6.5 inches (' + body + ' rows)';
       }
