70516f9abd6950d550e7b990bd0b0fb526a49b95
Liz Abinante <eabinante@gmail.com>
2014-08-19 18:32:35 -0500

    yo angular:controller hatCrownDecreases

---
+++ app/scripts/controllers/hatcrowndecreases.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name knitcalcApp.controller:HatcrowndecreasesCtrl
+ * @description
+ * # HatcrowndecreasesCtrl
+ * Controller of the knitcalcApp
+ */
+angular.module('knitcalcApp')
+  .controller('HatcrowndecreasesCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
